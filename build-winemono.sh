c6452375b27222ac20bcbd4a9c4399679f57e2c1
Vincent Povirk <vincent@codeweavers.com>
2019-02-26 09:18:02 -0600

    Add Theorafile to the build.

--- build-winemono.sh
+++ build-winemono.sh
@@ -125,6 +125,14 @@ cross_build_mono ()
     fi
     make $MAKEOPTS || exit 1
     cp "$BUILDDIR/build-cross-$ARCH/FAudio/FAudio.dll" "$BUILDDIR/image/bin/FAudio-$ARCH.dll" || exit 1
+	
+    if test ! -d "$BUILDDIR/build-cross-$ARCH/Theorafile"; then
+        mkdir "$BUILDDIR/build-cross-$ARCH/Theorafile"
+    fi
+
+	cd "$BUILDDIR/build-cross-$ARCH/Theorafile"
+	make $MAKEOPTS "CC=${MINGW}-gcc" -f "$SRCDIR/Theorafile/Makefile" || exit 1
+    cp "$BUILDDIR/build-cross-$ARCH/Theorafile/libtheorafile.dll" "$BUILDDIR/image/bin/libtheorafile-$ARCH.dll" || exit 1
 }
 
 build_cli ()
